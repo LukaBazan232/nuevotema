e927f9a579b0602b366f462c3091b5c2c858975e
lukabazan232 <lukabazan404@gmail.com>
2023-10-04 16:46:05 -0300

    hice cositas

--- rickymorty/src/components/Character/Character.jsx
+++ rickymorty/src/components/Character/Character.jsx
@@ -17,22 +17,48 @@ export const AllCharacters = () => {
         //useEffect y useState Hooks
     <>    
         
+        
+        <div className="CAJAGIGANTE">
+
+        <div className="botones">
+
+            <button className="botones2" > Docs </button>
+            <button className="botones2"> About </button>
+            <button > SUPPORT US </button>
+
+        </div>
+        <div className="titulo"> <h1> Rick and Morty API </h1> </div>
+
         <ul className="taper">
-    
+        
          {
+            
                 allCharacters.map((item,index) =>(
-                 <li key ={index} >
-                     <h3>{item.name}</h3>
-                     <p>{item.status}</p>
-                      <img src ={item.image} alt = ""/>
-                 </li> 
                     
+                    <li key ={index}  >
+                    <div className="taper2">
+                        <img src ={item.image} alt = ""/>
+                        <div className ="taper3">   
+                        <h3>{item.name}</h3>
+                        <p>{item.status}</p>
+                        </div>
+                    </div>   
+                 </li> 
+                 
              ))
          }
     
     
          </ul>
 
+         <div className="final">
+
+                
+
+
+
+         </div>
+         </div>
     </>     
     )
 
